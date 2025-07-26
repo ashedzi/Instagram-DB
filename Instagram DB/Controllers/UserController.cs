df12260c9ac5719f35a486132d8f9ebe45014bd7
Connor Hall <connrhall204@gmail.com>
2025-07-26 16:06:07 -0500

    Made everything work

---
+++ Instagram DB/Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Instagram_DB.BLL;
+using Instagram_DB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Instagram_DB.Controllers {
+    public class UserController : Controller {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService) {
+            _userService = userService;
+        }
+        public IActionResult Index () {
+            List<User> users = _userService.getUsers();
+            return View(users);
+        }
+    }
+}
