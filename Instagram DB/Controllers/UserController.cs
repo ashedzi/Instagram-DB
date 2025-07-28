09dec2a985e0e5e3dfcff3d67cf88d24089d7536
Ashedzi Solomon <solomonashe84@gmail.com>
2025-07-27 23:23:24 -0500

    create profile method in user controller

--- Instagram DB/Controllers/UserController.cs
+++ Instagram DB/Controllers/UserController.cs
@@ -5,13 +5,27 @@ using Microsoft.AspNetCore.Mvc;
 namespace Instagram_DB.Controllers {
     public class UserController : Controller {
         private readonly UserService _userService;
+        private readonly UserService _profile;
 
         public UserController(UserService userService) {
             _userService = userService;
         }
         public IActionResult Index () {
-            List<User> users = _userService.getUsers();
+            List<User> users = _userService.GetUsers();
             return View(users);
+        }
+
+        [HttpGet]
+        [Route("User/Profile/{UserName}")]
+        public IActionResult Profile(string UserName) {
+            User user = _userService.GetUsers().FirstOrDefault(u => u.Username.Equals(UserName, StringComparison.OrdinalIgnoreCase));
+
+            if (user == null) {
+                return NotFound();
+            }
+
+            return View(user);
+
         }
     }
 }
