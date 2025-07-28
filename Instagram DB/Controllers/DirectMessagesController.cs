bdc09c8da0efc78b082c0902a79ccb331662f2df
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-07-27 20:56:10 -0500

    created directmessagescontroller

---
+++ Instagram DB/Controllers/DirectMessagesController.cs
@@ -0,0 +1,31 @@
+﻿using Instagram_DB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Instagram_DB.Controllers {
+    public class DirectMessagesController : Controller {
+        private readonly InstagramDbContext _context;
+
+        public DirectMessagesController(InstagramDbContext context) {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(int? userId) {
+            if (userId == null) {
+                return BadRequest("User ID is required.");
+            }
+
+            var messages = await _context.DirectMessages
+                .Include(dm => dm.SenderUser)
+                .Include(dm => dm.ReceiverUser)
+                .Where(dm => dm.SenderUserId == userId || dm.ReceiverUserId == userId)
+                .OrderByDescending(dm => dm.Timestamp)
+                .ToListAsync();
+
+            ViewBag.UserId = userId;
+            return View(messages);
+        }
+    }
+}
