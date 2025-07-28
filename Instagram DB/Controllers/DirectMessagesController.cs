95f1d3d9593e2a3d6caca0ca747a1b055c43d02a
Sarah Mitchell <sarahmitchell17@gmail.com>
2025-07-27 21:08:47 -0500

    added delete dm function

--- Instagram DB/Controllers/DirectMessagesController.cs
+++ Instagram DB/Controllers/DirectMessagesController.cs
@@ -1,32 +1,33 @@
-﻿using Instagram_DB.Models;
+﻿using Instagram_DB.BLL;
+using Instagram_DB.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace Instagram_DB.Controllers {
     public class DirectMessagesController : Controller {
+        private readonly DirectMessageService _directMessageService;
         private readonly InstagramDbContext _context;
 
-        public DirectMessagesController(InstagramDbContext context) {
-            _context = context;
+        public DirectMessagesController(DirectMessageService directMessageService, InstagramDbContext context) {
+            _directMessageService = directMessageService;
+            _context = context; 
         }
 
-        public async Task<IActionResult> Index(int? userId) {
+        public IActionResult Index(int? userId) {
             if (userId == null) {
                 return BadRequest("User ID is required.");
             }
 
-            var messages = await _context.DirectMessages
-                .Include(dm => dm.SenderUser)
-                .Include(dm => dm.ReceiverUser)
+            var messages = _directMessageService.GetDirectMessages()
                 .Where(dm => dm.SenderUserId == userId || dm.ReceiverUserId == userId)
                 .OrderByDescending(dm => dm.Timestamp)
-                .ToListAsync();
+                .ToList();
 
             ViewBag.UserId = userId;
             return View(messages);
         }
+
         public IActionResult SendMessage(int? userId) {
             if (userId == null) {
                 return BadRequest("User ID is required.");
