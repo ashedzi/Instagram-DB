e84e9fddcde91cd062d1d56746076dd98e6bdddf
Ayomide Boye <boyeayomide@gmail.com>
2025-07-28 10:33:39 -0500

    Created PostController.cs and made changes to Index

---
+++ Instagram DB/Controllers/PostController.cs
@@ -0,0 +1,23 @@
+﻿using Instagram_DB.BLL;
+using Instagram_DB.DAL;
+using Instagram_DB.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Instagram_DB.Controllers {
+    public class PostController : Controller {
+
+        private readonly PostService _postService;
+        private readonly InstagramDbContext _context;
+
+        public PostController() {
+            _context = new InstagramDbContext();
+            PostRepository postRepository = new PostRepository(_context);
+            _postService = new PostService(postRepository);
+        }
+
+        public IActionResult Index() {
+            List<Post> posts = _postService.GetPosts();
+            return View(posts);
+        }
+    }
+}
