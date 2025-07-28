3aef3fb773109c008c999afdcbba50b9de4428cf
Ayomide Boye <boyeayomide@gmail.com>
2025-07-28 13:36:36 -0500

    started post controller again with a diff style that i understand better

--- Instagram DB/Controllers/PostController.cs
+++ Instagram DB/Controllers/PostController.cs
@@ -1,76 +1,31 @@
-﻿using Instagram_DB.BLL;
-using Instagram_DB.DAL;
-using Instagram_DB.Models;
+﻿using Instagram_DB.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Instagram_DB.Controllers {
-    public class PostController : Controller {
-
-        private readonly PostService _postService;
+    public class PostsController : Controller {
         private readonly InstagramDbContext _context;
 
-        public PostController() {
-            _context = new InstagramDbContext();
-            PostRepository postRepository = new PostRepository(_context);
-            _postService = new PostService(postRepository);
+        public PostsController(InstagramDbContext context) {
+            _context = context;
         }
 
-        public IActionResult Index() {
-            List<Post> posts = _postService.GetPosts();
-            return View(posts);
-        }
+        public async Task<IActionResult> Index(int? userId) {
+            var posts = _context.Posts
+                .Include(p => p.User)
+                .Include(p => p.Comments)
+                .OrderByDescending(p => p.Timestamp)
+                .AsQueryable();
 
-        public IActionResult Details(string id) {
-            if (String.IsNullOrEmpty(id)) {
-                return BadRequest();
+            if (userId != null) {
+                posts = posts.Where(p => p.UserId == userId);
+                ViewBag.UserId = userId;
             }
 
-            Post post = _postService.GetPosts()
-                .FirstOrDefault(p => p.PostId == id);
-
-            if (post == null) {
-                return NotFound();
-            }
-
-            List<Comment> comments = _context.Comments
-                .Where(c => c.PostId == id)
-                .OrderByDescending(c => c.Timestamp)
-                .ToList();
-
-            List<Like> likes = _context.Likes
-                .Where(l => l.PostId == id)
-                .ToList();
-
-            ViewBag.Comments = comments;
-            ViewBag.Likes = likes;
-
-            return View(post);
-        }
-
-        [HttpPost]
-        public IActionResult AddComment(string postId, string content, int userId) {
-            if (String.IsNullOrWhiteSpace(content) || String.IsNullOrWhiteSpace(postId)) {
-                return RedirectToAction("Details", new { id = postId });
-            }
-
-            Post post = _context.Posts.FirstOrDefault(p => p.PostId == postId);
-            if (post == null) {
-                return NotFound();
-            }
-
-            Comment newComment = new Comment {
-                PostId = postId,
-                CommenterUserId = userId,    
-                PosterUserId = post.UserId, 
-                Content = content,
-                Timestamp = DateTime.Now,
-                Likes = 0,
-            };
-
-            _context.Comments.Add(newComment);
-            _context.SaveChanges();
-
-            return RedirectToAction("Details", new { id = postId });
+            return View(await posts.ToListAsync());
         }
 
     }
