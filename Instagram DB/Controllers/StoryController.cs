e201f41ce1131a3ff1880f53faf1de58b6c9a2fa
Haodi Hua <huahodi2014@gmail.com>
2025-07-29 16:49:14 -0500

    Edit storyController #5

--- Instagram DB/Controllers/StoryController.cs
+++ Instagram DB/Controllers/StoryController.cs
@@ -22,3 +22,40 @@ namespace Instagram_DB.Controllers {
                 .ToList();
             return View(stories);
         }
+        public IActionResult Details ( string id ) {
+            var story = _context.Stories
+                .Where(s => s.StoryId == id)
+                .Select(s => new Story {
+                    StoryId = s.StoryId,
+                    Caption = s.Caption,
+                    Image = s.Image,
+                    Timestamp = s.Timestamp,
+                    PosterNavigation = s.PosterNavigation,
+                    ViewerNavigation = s.ViewerNavigation
+                })
+                .FirstOrDefault();
+
+            if (story == null)
+                return NotFound();
+
+            var likes = _context.StoryLikes
+                .Where(l => l.StoryId == id)
+                .Select(l => new StoryLike {
+                    StoryId = l.StoryId,
+                    Liker = l.Liker,
+                    Timestamp = l.Timestamp,
+                    LikerNavigation = l.LikerNavigation
+                })
+                .ToList();
+
+            var viewers = new List<User>();
+            if (story.ViewerNavigation != null)
+                viewers.Add(story.ViewerNavigation);
+
+            ViewBag.StoryLikes = likes;
+            ViewBag.StoryViewers = viewers;
+
+            return View(story);
+        }
+    }
+}
