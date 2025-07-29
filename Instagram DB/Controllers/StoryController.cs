beae7d47b92cb106ecb1db7292b9c196333ca6bb
Haodi Hua <huahodi2014@gmail.com>
2025-07-29 14:01:46 -0500

    Create Controller

---
+++ Instagram DB/Controllers/StoryController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Instagram_DB.Controllers {
+    public class StoryController : Controller {
+        public IActionResult Index () {
+            return View();
+        }
+    }
+}
