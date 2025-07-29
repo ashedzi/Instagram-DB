3a6376333d5e4a70d79f7179b75d02f3f3c20b1c
Connor Hall <connrhall204@gmail.com>
2025-07-29 17:33:20 -0500

    More progress

--- Instagram DB/Controllers/UserController.cs
+++ Instagram DB/Controllers/UserController.cs
@@ -5,17 +5,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace Instagram_DB.Controllers {
     public class UserController : Controller {
         private readonly UserService _userService;
-        //private readonly FollowerService _followerService;
 
         public UserController(UserService userService) {
             _userService = userService;
-            //_followerService = followerService;
         }
 
         public IActionResult Index () {
             List<User> users = _userService.GetUsers();
-            //List<Follower> follows = _followerService.GetFollowers();
-            return View();
+            return View(users);
         }
 
         [HttpGet]
@@ -30,27 +27,16 @@ namespace Instagram_DB.Controllers {
             return View(user);
         }
 
-        //[HttpGet]
-        //[Route("User/Followers/{UserName}")]
-        //public IActionResult Followers(string UserName) {
-        //    List<User> users = _userService.GetUsers();
-        //    List<Follower> follows = _followerService.GetFollowers();
+        [HttpGet]
+        public IActionResult Followers(string UserName) {
+            User user = _userService.GetUsers().FirstOrDefault(u => u.Username.Equals(UserName, StringComparison.OrdinalIgnoreCase));
 
-        //    User user = _userService.GetUsers().FirstOrDefault(u => u.Username.Equals(UserName, StringComparison.OrdinalIgnoreCase));
-        //    if (user == null) {
-        //        return NotFound();
-        //    }
-
-        //    var follow = users.Join(follows,
-        //        users => users.UserId,
-        //        follows => follows.FollowingUserId,
-        //        (u, f) => new {
-        //            u.UserId,
-        //            u.Username,
-        //            u.ProfilePic,
-        //        });
-        //    return View(follow);
-        //}
+            if (user == null) {
+                return NotFound();
+            }
+            List<User> followers = user.FollowerUsers.ToList();
+            return View(followers);
+        }
 
         [HttpGet]
         public IActionResult Following (string UserName) {
