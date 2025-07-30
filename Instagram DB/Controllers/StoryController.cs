92da73f327062b9e34a4e4d7cf9cbf30ebf06d7e
Haodi Hua <huahodi2014@gmail.com>
2025-07-29 21:05:02 -0500

    Edit controller #6

--- Instagram DB/Controllers/StoryController.cs
+++ Instagram DB/Controllers/StoryController.cs
@@ -7,11 +7,11 @@ namespace Instagram_DB.Controllers {
     public class StoryController : Controller {
         private readonly InstagramDbContext _context;
 
-        public StoryController(InstagramDbContext context) {
+        public StoryController ( InstagramDbContext context ) {
             _context = context;
         }
 
-        public IActionResult Index() {
+        public IActionResult Index () {
             var stories = _context.Stories
                 .Select(s => new Story {
                     StoryId = s.StoryId,
@@ -24,7 +24,7 @@ namespace Instagram_DB.Controllers {
             return View(stories);
         }
 
-        public IActionResult Details(string id) {
+        public IActionResult Details ( string id ) {
             var story = _context.Stories
                 .Where(s => s.StoryId == id)
                 .Select(s => new Story {
@@ -61,7 +61,7 @@ namespace Instagram_DB.Controllers {
         }
 
         [HttpPost]
-        public IActionResult LikeStory(string id, int likerId) {
+        public IActionResult LikeStory ( string id, int likerId ) {
             var exists = _context.StoryLikes.Any(l => l.StoryId == id && l.Liker == likerId);
             if (!exists) {
                 var newLike = new StoryLike {
@@ -73,6 +73,37 @@ namespace Instagram_DB.Controllers {
                 _context.SaveChanges();
             }
             return RedirectToAction("Details", new { id });
+        }
+
+        [HttpGet]
+        public IActionResult Delete ( string id ) {
+            var story = _context.Stories
+                .Select(s => new Story {
+                    StoryId = s.StoryId,
+                    Caption = s.Caption,
+                    Image = s.Image,
+                    Timestamp = s.Timestamp,
+                    PosterNavigation = s.PosterNavigation
+                })
+                .FirstOrDefault(s => s.StoryId == id);
+
+            if (story == null)
+                return NotFound();
+
+            return View(story);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed ( string id ) {
+            var story = _context.Stories.FirstOrDefault(s => s.StoryId == id);
+            if (story != null) {
+                var likes = _context.StoryLikes.Where(l => l.StoryId == id);
+                _context.StoryLikes.RemoveRange(likes);
+
+                _context.Stories.Remove(story);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
 
     }
